fa3d59371264e716c97066afa932e9723b73a624
hie3-336 <tomohiko336@gmail.com>
2025-04-29 14:22:58 +0900

    一旦全キャラクターの情報をAPIから取得できるよう設定

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log("Hello World");
+
+async function main() {
+  try {
+    const charaInfo = await fetchCharaInfo();
+    console.log(charaInfo);
+  } catch (error) {
+    console.error(`エラーが発生しました (${error})`);
+  }
+}
+
+function fetchCharaInfo (){
+  return fetch(`https://ihatov08.github.io/kimetsu_api/api/all.json`)
+  .then(response => {
+    if (!response.ok) {
+      return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
+    } else {
+      return response.json();
+    }
+  });
+}
+
+main();
