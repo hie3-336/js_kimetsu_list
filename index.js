9ea75a05dc2849911ea0850a8925f0365b3ca31f
hie3-336 <tomohiko336@gmail.com>
2025-04-29 20:03:05 +0900

    全キャラクターの情報を表示できるように設定

--- index.js
+++ index.js
@@ -1,12 +1,50 @@
 console.log("Hello World");
 
+const contentArea = document.getElementById("result");
+
 async function main() {
+  
   try {
     const charaInfo = await fetchCharaInfo();
     console.log(charaInfo);
+
+    for (const index in charaInfo) {
+      console.log(charaInfo[index].category);
+      console.log(charaInfo[index].image);
+      console.log(charaInfo[index].name);
+
+      const charaElement = document.createElement('div');
+      const imgElement = document.createElement('img');
+      const categoryElement = document.createElement('div');
+      const nameElement = document.createElement('div');
+
+      imgElement.src = "https://ihatov08.github.io"+ charaInfo[index].image; // 画像パス
+      imgElement.alt = charaInfo[index].name;
+      imgElement.width = 200;
+      imgElement.height = 200;
+
+      categoryElement.innerText = charaInfo[index].category;
+      nameElement.innerText = charaInfo[index].name;
+
+      charaElement.appendChild(imgElement);
+      charaElement.appendChild(categoryElement);
+      charaElement.appendChild(nameElement);
+      contentArea.appendChild(charaElement);
+
+    }
   } catch (error) {
     console.error(`エラーが発生しました (${error})`);
   }
+
+  
+  // let img_element2 = document.createElement('img');
+  // img_element2.src = 'Hakubisin.jpeg'; // 画像パス
+  // img_element2.alt = 'ハクビシン'; // 代替テキスト
+  // img_element2.width = 200; // 横サイズ（px）
+  // img_element2.height = 200; // 縦サイズ（px）
+  // // 指定した要素にimg要素を挿入
+
+  // content_area.appendChild(img_element2);
 }
 
 function fetchCharaInfo (){
